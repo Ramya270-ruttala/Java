7c4ccc878b759e32b4f42545b8875387c653ce3e
Ramya270-ruttala <112839230+Ramya270-ruttala@users.noreply.github.com>
2026-04-18 18:22:06 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,5 +1,7 @@
 public class Example{
     public static void main(String[] args){
-        System.out.println("Java language")
+        System.out.println("Java language");
+        int a=3+4
+        System.out.prinln(a);
     }
 }
